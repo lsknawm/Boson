17313ad9f19bd77cff03add298e8032c62f529a8
lsknawm <lsknawm@gmail.com>
2026-01-05 00:13:39 +0800

    INIT

--- conf/config.go
+++
@@ -1,7 +0,0 @@
-package conf
-
-const (
-	MongoURI = "mongodb://localhost:27017"
-	DBName   = "quanta_db"
-	CollName = "data"
-)
--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"Boson/database"
-	"Boson/router"
 	"fmt"
 )
 
 func main() {
-	// 1. 初始化数据库
-	database.InitDB()
-
-	// 2. 设置路由并启动服务
-	r := router.SetupRouter()
-
-	// 3. 启动服务
-	fmt.Println("Server is running on port 8080")
-	r.Run(":8080")
+	fmt.Println("Hello, World!")
 }
